b8a075664b4672671a522748282d6ab89efb99b7
jscamposx <jscamposx@gmail.com>
2025-04-12 23:00:27 -0700

    Congiguracion para agregar redis

--- read-api/src/main/java/com/read_api/service/UrlService.java
+++ read-api/src/main/java/com/read_api/service/UrlService.java
@@ -2,6 +2,7 @@ package com.read_api.service;
 
 import com.read_api.model.Url;
 import com.read_api.repository.UrlRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,9 +14,14 @@ public class UrlService {
         this.urlRepository = urlRepository;
     }
 
+
+    @Cacheable(value = "longUrls", key = "#shortUrl")
     public String getLongUrl(String shortUrl) {
         return urlRepository.findById(shortUrl)
                 .map(Url::getLongUrl)
                 .orElseThrow(() -> new RuntimeException("URL no encontrada"));
+
+
     }
+
 }
